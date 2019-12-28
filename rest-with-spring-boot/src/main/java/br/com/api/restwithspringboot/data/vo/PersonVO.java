4f782adc076de39cf32a1ef28d6d164fe9e8d7c0
Gabriel Rezende <gabriel.rezendeps@gmail.com>
2019-12-28 16:11:32 -0300

    Segundo commit

--- rest-with-spring-boot/src/main/java/br/com/api/restwithspringboot/data/vo/PersonVO.java
+++ rest-with-spring-boot/src/main/java/br/com/api/restwithspringboot/data/vo/PersonVO.java
@@ -2,6 +2,10 @@ package br.com.api.restwithspringboot.data.vo;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,14 +13,22 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
 public class PersonVO implements Serializable{
 	
 	private static final long serialVersionUID = 7656607858946969329L;
 
 	private Long id;
+	
+	@JsonProperty("first_name")
 	private String firstName;
+	
+	@JsonProperty("last_name")
 	private String lastName;
 	private String address;
+	
+	//@JsonIgnore
+	//This annotation ignore the attribute on response
 	private String gender;
 
 }
